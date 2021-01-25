8df75de1bcb4c896f0928f428533d210ec6c891f
Bryan <bryanlima200@outlook.com>
2021-01-25 16:04:55 -0300

    Capítulo 03: Add ex07

---
+++ capitulo-03/ex07.py
@@ -0,0 +1,6 @@
+# Faça um programa que peça dois números inteiros
+# Imprima a soma desses dois números na tela
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+print(f'\nA soma entre {n1} e {n2} é igual a {n1 + n2}')
