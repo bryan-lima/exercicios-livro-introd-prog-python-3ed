80eefe5aa7c5ccd3e8212353497e257e4251b4ec
Bryan <bryanlima200@outlook.com>
2021-01-25 13:03:19 -0300

    Capítulo 02: Add ex01

---
+++ capitulo-02/ex01.py
@@ -0,0 +1,8 @@
+# Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador Python
+# 10 + 20 x 30
+# 4² ÷ 30
+# (9**4 + 2) x 6 - 1
+
+print(10 + 20 * 30)
+print(4 ** 2 / 30)
+print((9 ** 4 + 2) * 6 - 1)
