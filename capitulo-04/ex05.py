d1f846d8d1c0ae55c5990fe8014bf1ab764d9b68
Bryan <bryanlima200@outlook.com>
2021-01-25 19:00:03 -0300

    Capítulo 04: Add ex05

---
+++ capitulo-04/ex05.py
@@ -0,0 +1,11 @@
+# Execute o Programa 4.4 e experimente alguns valores
+# Verifique se os resultados foram os mesmo do Programa 4.2
+
+# Programa 4.4 do livro, página 79
+# Programa 4.4 - Carro novo ou velho, dependendo da idade com else
+
+age = int(input('Digite a idade de seu carro: '))
+if age <= 3:
+    print('Seu carro é novo')
+else:
+    print('Seu carro é velho')
