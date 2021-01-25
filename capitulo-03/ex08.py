ef12a0fd173ffafa8591128cdd8cefe485cfdcc5
Bryan <bryanlima200@outlook.com>
2021-01-25 16:09:29 -0300

    Capítulo 03: Add ex08

---
+++ capitulo-03/ex08.py
@@ -0,0 +1,5 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em milímetros
+
+meters = float(input('Digite um valor (metros): '))
+millimeter = meters * 1000
+print(f'{meters} metro(s) convertido para milímetros é igual a {millimeter:.2f}')
