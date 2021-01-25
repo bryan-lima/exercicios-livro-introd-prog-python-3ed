da4b2752704db70d0f4ae44f709f9531d78cb359
Bryan <bryanlima200@outlook.com>
2021-01-25 17:38:32 -0300

    Capítulo 04: Add ex01

---
+++ capitulo-04/ex01.py
@@ -0,0 +1,13 @@
+# Analise o programa 4.1
+# Responda o que acontece se o primeiro e o segundo valores forem iguais? Explique
+# → Resposta: Nada será impresso
+
+# Programa 4.1 do livro, página 75
+# Programa 4.1 - Lê dois valores e imprime qual é o maior
+
+a = int(input('Primeiro valor: '))
+b = int(input('Segundo valor: '))
+if a > b:
+    print('O primeiro valor é o maior!')
+if b > a:
+    print('O segundo valor é o maior!')
