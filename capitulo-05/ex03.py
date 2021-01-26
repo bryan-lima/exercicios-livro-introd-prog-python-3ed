5a3f5b244df403f3f26ad976706bd6937dd80a5b
Bryan <bryanlima200@outlook.com>
2021-01-25 22:22:07 -0300

    Capítulo 05: Add ex03

---
+++ capitulo-05/ex03.py
@@ -0,0 +1,9 @@
+# Faça um programa para escrever a contagem regressiva do lançamento de um foguete
+# O programa deve imprimir 10, 9, 8, ..., 1, 0 e Fogo! na tela
+
+n = 10
+
+while n >= 0:
+    print(n)
+    n -= 1
+print('Fogo!')
