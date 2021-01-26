8d207b30a6770a21aae3aaabc9c2713ab9658504
Bryan <bryanlima200@outlook.com>
2021-01-25 22:34:10 -0300

    Capítulo 05: Add ex04

---
+++ capitulo-05/ex04.py
@@ -0,0 +1,7 @@
+# Modifique o programa anterior para imprimir de 1 até o número digitado pelo usuário, mas, dessa vez, apenas os números ímpares
+
+lastNumber  = int(input('Digite o último número a imprimir: '))
+n = 1
+while n <= lastNumber:
+    print(n)
+    n += 2
