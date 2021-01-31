434ca594341ce0ce705972a7c27ad4f57ef18932
Bryan <bryanlima200@outlook.com>
2021-01-31 20:42:53 -0300

    Capítulo 09: Add ex15

---
+++ capitulo-09/ex15.py
@@ -0,0 +1,130 @@
+# Altere o Programa 7.2, o jogo da forca
+# Utilize um arquivo em que uma palavra seja gravada a cada linha
+# Use um editor de textos para gerar o arquivo
+# Ao iniciar o programa, utilize esse arquivo para carregar (ler) a lista de palavras
+# Experimente também perguntar o nome do jogador e gerar um arquivo com o número de acertos dos cinco melhores
+
+import sys
+import random
+
+FILE_SCOREBOARD = 'placar.txt'
+FILE_WORDS_LIST = 'palavras.txt'
+
+wordsList = []
+scoreboardDict = {}
+
+
+def load_words():
+    try:
+        file = open(FILE_WORDS_LIST, 'r', encoding='utf-8')
+    except FileNotFoundError:
+        print(f'\n\nArquivo "{FILE_WORDS_LIST}" não encontrado!')
+        print(f'Para jogar, crie um arquivo de palavras com nome "{FILE_WORDS_LIST}", contendo uma palavra por linha.\n\n')
+        sys.exit(1)
+    for word in file.readlines():
+        word = word.strip().lower()
+        if word != '':
+            wordsList.append(word)
+    file.close()
+
+
+def load_scoreboard():
+    try:
+        file = open(FILE_SCOREBOARD, 'r+')
+    except FileNotFoundError:
+        file = open(FILE_SCOREBOARD, 'w+')
+    for line in file.readlines():
+        line = line.strip()
+        if line != '':
+            user, counter = line.split(';')
+            scoreboardDict[user] = int(counter)
+    file.close()
+
+
+def save_scoreboard():
+    file = open(FILE_SCOREBOARD, 'w', encoding='utf-8')
+    for user in scoreboardDict.keys():
+        file.write(f'{user};{scoreboardDict[user]}\n')
+    file.close()
+
+
+def update_scoreboard(user):
+    if user in scoreboardDict:
+        scoreboardDict[user] += 1
+    else:
+        scoreboardDict[user] = 1
+    save_scoreboard()
+
+
+def show_scoreboard():
+    scoreboardOrdered = []
+    for user, score in scoreboardDict.items():
+        scoreboardOrdered.append([user, score])
+    scoreboardOrdered.sort(key=lambda score: score[1])
+    print('\n\nMelhores jogadores por número de acertos:')
+    scoreboardOrdered.reverse()
+    for up in scoreboardOrdered:
+        print(f'{up[0]:30s} {up[1]:10d}')
+
+
+load_words()
+load_scoreboard()
+
+word = wordsList[random.randint(0, len(wordsList)-1)]
+
+typed = []
+hits = []
+errors = 0
+
+while True:
+    password = ''
+
+    for letter in word:
+        password += letter if letter in hits else '_'
+    print(password)
+
+    if password == word:
+        print('Você acertou!')
+        name = input('Digite seu nome: ')
+        update_scoreboard(name)
+        break
+
+    attempt = input('\nDigite uma letra: ').lower().strip()
+
+    if attempt in typed:
+        print('Você já tentou esta letra!')
+        continue
+    else:
+        typed += attempt
+        if attempt in word:
+            hits += attempt
+        else:
+            errors += 1
+            print('Você errou!')
+
+    print('X==:==\nX  :   ')
+    print('X  O   ' if errors >= 1 else 'X')
+
+    line2 = ''
+    if errors == 2:
+        line2 = r'  |   '
+    elif errors == 3:
+        line2 = r' \|   '
+    elif errors >= 4:
+        line2 = r' \|/ '
+    print(f'X{line2}')
+
+    line3 = ''
+    if errors == 5:
+        line3 += r' /     '
+    elif errors >= 6:
+        line3 += r' / \ '
+    print(f'X{line3}')
+
+    print('X\n===========')
+
+    if errors == 6:
+        print('Enforcado!')
+        break
+
+show_scoreboard()
